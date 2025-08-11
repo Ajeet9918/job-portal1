af323085530a2d7531977a35207fedaa2cf17150
Ajeet9918 <mrajeetkumargupta8840@gmail.com>
2025-08-11 09:42:21 +0530

    url

--- Frontend/api.js
+++ Frontend/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://job-portal1-rk3e.onrender.com/api';
 
 // Set auth token
 const setAuthToken = token => {
